a320682341fb11c8e054b99edfc82d320bc3c1a3
kushagrasinghfly <skushagra615@gmail.com>
2025-02-07 13:38:51 +0530

    Update validParenthesis.cpp

--- validParenthesis.cpp
+++ validParenthesis.cpp
@@ -5,20 +5,19 @@ using namespace std;
 bool isValidParentheses(string expr) {
     stack<char> st;
     
-    for (char ch : expr) {
-        if (ch == '(' || ch == '{' || ch == '[') {
+    for (char ch : expr) 
+    {
+        if (ch == '(' || ch == '{' || ch == '[')  //case 1
             st.push(ch);
-        } else if (ch == ')' || ch == '}' || ch == ']') {
-            if (st.empty()) return false;
+        else if (ch == ')' || ch == '}' || ch == ']')  //case 2
+        {
+            if (st.empty()) return false;           //if brackets end and stack is empty return false cause its valid
             
             char top = st.top();
-            if ((ch == ')' && top == '(') ||
-                (ch == '}' && top == '{') ||
-                (ch == ']' && top == '[')) {
-                st.pop();
-            } else {
+            if ((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')) 
+                st.pop(); 
+            else 
                 return false;
-            }
         }
     }
     
@@ -30,11 +29,9 @@ int main() {
     cout << "Enter an expression: ";
     cin >> expr;
     
-    if (isValidParentheses(expr)) {
+    if (isValidParentheses(expr)) 
         cout << "Valid Parentheses" << endl;
-    } else {
+    else 
         cout << "Invalid Parentheses" << endl;
-    }
-    
     return 0;
 }
