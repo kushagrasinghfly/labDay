11625b712714e2617b3fdc326347308743615cb8
kushagrasinghfly <skushagra615@gmail.com>
2025-03-28 12:48:15 +0530

    Add files via upload

---
+++ learnAVL.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+class Node 
+{
+  public:
+      int key;
+      Node* left;
+      Node* right;
+      int height;
+      Node(int k) 
+      {
+          key = k;
+          left = right = nullptr;
+          height = 1;
+      }
+};
+
+int getHeight(Node* node) 
+{
+    return node ? node->height : 0;
+}
+
+int getBalanceFactor(Node* node) 
+{
+    return node ? getHeight(node->left) - getHeight(node->right) : 0;
+}
+
+Node* rightRotate(Node* y) 
+{
+    Node* x = y->left;
+    Node* T2 = x->right;
+    x->right = y;
+    y->left = T2;
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    return x;
+}
+
+Node* leftRotate(Node* x) 
+{
+    Node* y = x->right;
+    Node* T2 = y->left;
+    y->left = x;
+    x->right = T2;
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    return y;
+}
+
+Node* insert(Node* root, int key) 
+{
+    if (!root) return new Node(key);
+    if (key < root->key)
+        root->left = insert(root->left, key);
+    else if (key > root->key)
+        root->right = insert(root->right, key);
+    else
+        return root;
+
+    root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+    int balance = getBalanceFactor(root);
+
+    if (balance > 1 && key < root->left->key)
+        return rightRotate(root);
+    if (balance < -1 && key > root->right->key)
+        return leftRotate(root);
+    if (balance > 1 && key > root->left->key) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+    if (balance < -1 && key < root->right->key) {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+    return root;
+}
+
+void preOrder(Node* root) {
+    if (root) {
+        cout << root->key << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
+    cout << "Preorder traversal of AVL tree: ";
+    preOrder(root);
+    return 0;
+}
